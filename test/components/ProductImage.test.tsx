48dc95540652b4f2ee8d3b0cad5bd92c9814c019
Andrés Cortés <andreees.cortes@gmail.com>
2021-12-28 12:03:53 -0600

    Version 0.0.1 ready

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components/';
+import { product2 } from '../data/products';
+
+describe('ProductImage', () => {
+
+    test('Debe mostrar el componente correctamente con la imagen personalizada.', () => {
+
+        const wrapper = renderer.create(
+            <ProductImage img="https://hola.test" className="custom-class" />
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar el componente con la imagen del producto.', () => {
+        const wrapper = renderer.create(
+            <ProductCard
+                product={product2}
+            >
+                {
+                    () => (
+                        <ProductImage />
+                    )
+                }
+            </ProductCard>
+        )
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    })
+
+})
